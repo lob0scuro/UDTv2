fa5bf571060288462e323c117c658779a9304e29
lob0scuro <kamrin717@gmail.com>
2024-02-06 01:40:11 -0600

    finished editor, table is succesfully altered, working on home page now.

--- UDTv2/__init__.py
+++ UDTv2/__init__.py
@@ -2,16 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
 from flask_migrate import Migrate
-from instance import config
-# with open("/etc/config.json", encoding="utf-8") as config_file:
-#     config = json.load(config_file)
+# from instance import config
+with open("/etc/config.json", encoding="utf-8") as config_file:
+    config = json.load(config_file)
 db = SQLAlchemy()
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config['SQLALCHEMY_DATABASE_URI'] = config.figs["SQLALCHEMY_DATABASE_URI"]
-    app.config['SECRET_KEY'] = config.figs["SECRET_KEY"]
+    app.config['SQLALCHEMY_DATABASE_URI'] = config["SQLALCHEMY_DATABASE_URI"]
+    app.config['SECRET_KEY'] = config["SECRET_KEY"]
     with app.app_context():
         db.init_app(app)
     migrate = Migrate(app, db)
--- UDTv2/main.py
+++ UDTv2/main.py
@@ -11,9 +11,22 @@ mainBP = Blueprint('main', __name__)
 @mainBP.route('/')
 @login_required
 def index():
+    sites = Sites.query.all()
+
+
     if not g.user:
         redirect(url_for('auth.login'))
-    return render_template('main/index.html')
+    return render_template('main/index.html', sites=sites)
+
+@mainBP.route('/home-results')
+def home_results():
+    q = request.args.get("q")
+    if q:
+        results = Sites.query.filter(Sites.siteID.icontains(q) | Sites.siteName.icontains(
+            q) | Sites.parish.icontains(q) | Sites.filters.icontains(q) | Sites.city.icontains(q)).all()
+    else:
+        results = Sites.query.limit(15)
+    return render_template('main/search-results-home.html', results=results)
 
 
 @mainBP.route('/create', methods=('GET', 'POST'))
@@ -58,9 +71,9 @@ def create():
     return render_template('main/create.html')
 
 
-@mainBP.route('/read')
+@mainBP.route('/search')
 @login_required
-def read():
+def search():
     return render_template('main/search.html')
 
 
@@ -79,16 +92,36 @@ def results():
 @mainBP.route('/editor/<id>', methods=('GET', 'POST'))
 @login_required
 def editor(id):
-    db.session.query()
     data = Sites.query.get(id)
-    
     if request.method == 'POST':
-        pass
+        try:
+            data.siteID = request.form['siteID']
+            data.siteName = request.form['siteName']
+            data.address = request.form['address']
+            data.city = request.form['city']
+            data.state = request.form['state']
+            data.parish = request.form['parish']
+            data.coordinates = request.form['coordinates']
+            data.type_of = request.form['typeOf']
+            data.owner = request.form['owner']
+
+            data.manufacturer = request.form['manufacturer']
+            data.model = request.form['model']
+            data.serial = request.form['serial']
+            data.filters = request.form['filters']
+            data.refrigerant = request.form['freon']
+            data.controller = request.form['controller']
+
+            db.session.commit()
+            return redirect(url_for('main.search'))
+        except Exception as e:
+            flash(f"Error: {e}")
+            db.session.rollback()
+
+
     if not data:
-        redirect('found404.html')
-    else:
-        pass
-        
+        return redirect(url_for('main.found404'))
+
     return render_template('main/editor.html', data=data)
 
 
@@ -96,3 +129,8 @@ def editor(id):
 @login_required
 def map():
     return render_template('main/map.html')
+
+
+@mainBP.route('/found404')
+def found404():
+    return render_template('found404.html')
